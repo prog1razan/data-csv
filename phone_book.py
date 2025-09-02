523b0037c8762442063d17fe561aad7fd6aeffba
Razan Alothaim <razan1razan@gmail.com>
2025-09-02 11:44:31 +0300

    Refactor phone_book.py to use DictReader and print last name + phone number

--- phone_book.py
+++ phone_book.py
@@ -1,1 +1,9 @@
-
+import csv
+# open the CSV file safely and close it automatically when done
+with open('data/phone_book.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+# iterate over each row (as a dictionary) in the CSV
+    for row in csv_reader:
+        last_name = row["last_name"]      # use column name
+        phone_number = row["phone_number"]      # use column name
+        print(f"{last_name}: {phone_number}")
